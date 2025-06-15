f392a01c6f931edb2d4a30823cd229b8c364f59c
Douglas Alves <douglas-av@hotmail.com>
2025-06-15 00:39:05 -0300

    Eureka Server

---
+++ serviceregistry/src/test/kotlin/com/loja/serviceregistry/ServiceregistryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.loja.serviceregistry
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServiceregistryApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
